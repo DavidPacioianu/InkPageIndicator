402d2ee910535efc42a12b40141a0e911f505627
Vedant Agarwala <mail@vedant-agarwala.com>
2016-02-20 21:30:17 +0530

    Fixes Issue#7 and layout renderer error
    Ref: https://github.com/DavidPacioianu/InkPageIndicator/issues/7

--- InkPageIndicator/src/main/java/com/pixelcan/inkpageindicator/InkPageIndicator.java
+++ InkPageIndicator/src/main/java/com/pixelcan/inkpageindicator/InkPageIndicator.java
@@ -248,7 +248,7 @@ public class InkPageIndicator extends View implements ViewPager.OnPageChangeList
         } else {
             currentPage = 0;
         }
-        if (dotCenterX != null) {
+        if (dotCenterX != null && dotCenterX.length > 0 && (moveAnimation == null || !moveAnimation.isStarted())) {
             selectedDotX = dotCenterX[currentPage];
         }
     }
